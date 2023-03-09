d6e1901d6b8fc7013d3453ac120af82aa9dddea6
eidostern1 <93473053+eidostern1@users.noreply.github.com>
2023-03-09 14:52:38 +0200

    fixed small issue

--- solMatchFinalProject/app/src/main/java/Model/sysData.java
+++ solMatchFinalProject/app/src/main/java/Model/sysData.java
@@ -1,17 +1,6 @@
 package Model;
 
-import static android.content.ContentValues.TAG;
-
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+/*import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,7 +30,7 @@ public class sysData {
         this.mAuth = mAuth;
     }
 
-    public void registerUser(String email, String password)
+   /* public void registerUser(String email, String password)
     {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -62,7 +51,6 @@ public class sysData {
                     }
                 });
 
-    }
+    }*/
 
 
-}
--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/MainActivity.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.solmatchfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
         setContentView(R.layout.activity_login);
     }
 }
