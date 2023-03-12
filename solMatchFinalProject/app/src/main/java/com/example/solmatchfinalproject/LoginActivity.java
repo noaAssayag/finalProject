0ee3919fa4b54cee41ac1a59593805bb0a79453a
Noa Assayag <Noa.assayag02@gmail.com>
2023-03-12 12:06:10 +0200

    connect firebase to app

--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/LoginActivity.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.solmatchfinalproject;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,29 +11,92 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView btn;
+    EditText inputEmail,inputpassword;
+    Button btnLogin;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView btn=(TextView)findViewById(R.id.textViewSignUp);
-        Button bt=findViewById(R.id.btnLogin);
-        System.out.println("hello");
+        btn=(TextView)findViewById(R.id.textViewSignUp);
+        inputEmail=findViewById(R.id.inputEmail);
+        inputpassword=findViewById(R.id.inputpassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCredentials();
+            }
+        });
 
-        bt.setOnClickListener(new View.OnClickListener(){
+        btn.setOnClickListener(new View.OnClickListener(){
            @Override
            public  void onClick(View v)
            {
-                System.out.println("hello");
                 Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);
                 finish();
            }
         });
+        mAuth= FirebaseAuth.getInstance();
+        mLoadingBar=new ProgressDialog(LoginActivity.this);
     }
+    private void checkCredentials() {
+        String email=inputEmail.getText().toString();
+        String password=inputpassword.getText().toString();
+
+        if(email.isEmpty() || !email.contains("@"))
+        {
+            showError(inputEmail,"Email in not valid!");
+        }
+        else if(password.isEmpty() || password.length()<7)
+        {
+            showError(inputpassword,"password must be 7 character");
+        }
+
+        else
+        {
+            mLoadingBar.setTitle("Login");
+            mLoadingBar.setMessage("please wait,while check your credentials");
+            mLoadingBar.setCanceledOnTouchOutside(false);
+            mLoadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        mLoadingBar.dismiss();
+                        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+    }
+
+    private static void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
+    }
+
 }
--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/RegisterActivity.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/RegisterActivity.java
@@ -1,20 +1,110 @@
 package com.example.solmatchfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    TextView btn;
+    private EditText inputUserName,inputEmail,inputPassword,inputRePassword;
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        TextView btn=findViewById(R.id.label);
-        btn.setOnClickListener(view -> startActivity(new Intent(RegisterActivity.this,LoginActivity.class)));
+        btn=findViewById(R.id.label);
+        inputUserName=findViewById(R.id.inputUserName);
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword =findViewById(R.id.inputPassword);
+        inputRePassword =findViewById(R.id.inputRePassword);
+        mAuth=FirebaseAuth.getInstance();
+        mLoadingBar=new ProgressDialog(RegisterActivity.this);
+        btnRegister=findViewById(R.id.btnRegister);
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCredentials();
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void checkCredentials() {
+        String userName=inputUserName.getText().toString();
+        String email=inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+        String rePassword=inputRePassword.getText().toString();
+
+        if(userName.isEmpty() || userName.length()<7)
+        {
+            showError(inputUserName,"Your username is not valid!");
+        }
+        else if(email.isEmpty() || !email.contains("@"))
+        {
+            showError(inputEmail,"Email in not valid!");
+        }
+        else if(password.isEmpty() || password.length()<7)
+        {
+            showError(inputPassword,"password must be 7 character");
+        }
+        else if(rePassword.isEmpty() || !rePassword.equals(password))
+        {
+            showError(inputRePassword,"Password not match!");
+        }
+        else
+        {
+           mLoadingBar.setTitle("Registeration");
+           mLoadingBar.setMessage("please wait,while check your credentials");
+           mLoadingBar.setCanceledOnTouchOutside(false);
+           mLoadingBar.show();
+
+           mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if(task.isSuccessful())
+                   {
+                       Toast.makeText(RegisterActivity.this,"Successfuly registration",Toast.LENGTH_SHORT).show();
+
+                       mLoadingBar.dismiss();
+                       Intent intent=new Intent(RegisterActivity.this,MainActivity.class);
+                       intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                       startActivity(intent);
+                   }
+                   else
+                   {
+                       Toast.makeText(RegisterActivity.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
+                   }
+               }
+           });
+        }
+    }
+
+    private static void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
