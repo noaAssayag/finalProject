f1445cbc1b87c1c33e3ea4bb682c166d6e57ca6b
Noa Assayag <Noa.assayag02@gmail.com>
2023-03-14 22:51:52 +0200

    add more page for register , just UI

--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/profileActivity.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/profileActivity.java
@@ -2,16 +2,13 @@ package com.example.solmatchfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class profileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, profileActivity.class);
-        startActivity(intent);
         setContentView(R.layout.activity_profile);
     }
 }
