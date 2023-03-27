19d080973b4b0552e12869bd61a6a9390732e09b
eidostern1 <93473053+eidostern1@users.noreply.github.com>
2023-03-27 14:08:13 +0300

    register and sign in changes

--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/facebookSignIn.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/facebookSignIn.java
@@ -7,8 +7,11 @@ import androidx.annotation.NonNull;
 import com.facebook.AccessToken;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -25,9 +28,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class facebookSignIn extends LoginActivity {
     private FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+
+    Spinner type,gender;
+    TextView dateCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +75,8 @@ public class facebookSignIn extends LoginActivity {
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
 
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -73,6 +86,25 @@ public class facebookSignIn extends LoginActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            firebaseDatabase=FirebaseDatabase.getInstance();
+                            reference = firebaseDatabase.getReference("Users");
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if (snapshot.hasChild(user.getUid()))
+                                    {
+                                        Intent intent = new Intent(facebookSignIn.this,profileActivity.class);
+
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+
 
                         } else {
                             // If sign in fails, display a message to the user.
