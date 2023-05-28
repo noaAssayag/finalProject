e34cfb8843ce0ade9c6881959d01fa3ca94622c9
eidostern1 <93473053+eidostern1@users.noreply.github.com>
2023-05-28 13:00:00 +0300

    fixed image show

--- solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/Hosts/allHosts.java
+++ solMatchFinalProject/app/src/main/java/com/example/solmatchfinalproject/Hosts/allHosts.java
@@ -17,8 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+
 
 import java.util.ArrayList;
 import java.util.List;
